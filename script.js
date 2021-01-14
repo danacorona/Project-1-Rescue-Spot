ae49009f4187b10c38094ebe676c6dee4f02b04f
Daniel <daniel.castelline@gmail.com>
2021-01-14 14:58:57 -0600

    city/state fields, basic js to test html

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+var citySearch = $(".citySearch");
+var stateSearch = $(".stateSearch");
+
 $("document").ready(function(){
     $("select").formSelect();
 
+    console.log(citySearch.val().trim());
+    console.log(stateSearch);
 
 })
