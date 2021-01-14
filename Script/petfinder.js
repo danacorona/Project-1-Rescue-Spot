c6194891eb7933d7951cd3ff630cab54d39341fc
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-14 13:49:17 -0600

    Fixed Loading Bug In petfinder.js.

--- Script/petfinder.js
+++ Script/petfinder.js
@@ -1,20 +1,22 @@
-// API Keys
-var petfinderKey = "SQ6UnllCHsLaZRcQkfninVeneIproVkudasiqT8gBYdpYAF9BA";
-var petfinderSecret = "Z2E41aprwiJbwOKRbfbuWvMKBFUk6jqFTQ9B12NA";
+$(document).ready(function () {
+    // API Keys
+    var petfinderKey = "SQ6UnllCHsLaZRcQkfninVeneIproVkudasiqT8gBYdpYAF9BA";
+    var petfinderSecret = "Z2E41aprwiJbwOKRbfbuWvMKBFUk6jqFTQ9B12NA";
 
-var location = "";
+    var location = "";
 
-// API Call functions
-var pf = new petfinder.Client({ apiKey: petfinderKey, secret: petfinderSecret });
+    // API Call functions
+    var pf = new petfinder.Client({ apiKey: petfinderKey, secret: petfinderSecret });
 
-pf.animal.search({ location: "Austin, Texas" })
-    .then(function (response) {
-        // Do something with `response.data.animals`
-        console.dir(response);
-        // console.log(response.data.animals);
-    })
-    .catch(function (error) {
-        // Handle the error
-        console.log(error);
-    });
+    pf.animal.search({ location: "Austin, Texas" })
+        .then(function (response) {
+            // Do something with `response.data.animals`
+            console.dir(response);
+            console.log(response.data.animals);
 
+        })
+        .catch(function (error) {
+            // Handle the error
+            console.log(error);
+        });
+})
