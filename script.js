dd56c9d992e7d10bcd32d81dce71ad9a904a8a8e
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-16 15:06:55 -0600

    Search Results Dynamically Render To Page.
    I'm using template strings to clear the dog info section and render completely new cards. Next I will use a loop to render the first 5 searches to the page.

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ $(document).ready(function () {
     var citySearch = $(".citySearch");
     var stateSearch = $(".stateSearch");
     var submitBtn = $("#submit-button");
+    var dogInfo = $("#dog-info-container");
 
     // Global Variables
+    // User search is the string from the city state submit form.
     var userSearch = "";
+    // These are the variables from the API call. 
+    // I'm declaring them in the global scope as an empty string
+    // due to scope issues with calling the function to populate the info cards inside the API call function
+    // If someone knows a better solution I would love some help on this. -Trevor
+    var name = "";
+    var age = "";
+    var gender = "";
+    var url = "";
+    var spayedNeutered = "";
+    var address = "";
+    var city = "";
+    var state = "";
+    var description = "";
+    var photoURL = "";
+
 
     // Petfinder API Keys
     var petfinderKey = "SQ6UnllCHsLaZRcQkfninVeneIproVkudasiqT8gBYdpYAF9BA";
@@ -17,31 +34,36 @@ $(document).ready(function () {
 
         pf.animal.search({ location: userSearch })
             .then(function (response) {
-                //// Original array to pull data. Can remove all console logs in function when data is working with dog cards
+                //// Original array to pull data. 
+                //// Can remove all console logs in function when data is working with dog cards
                 var animalsArr = response.data.animals;
                 console.dir(animalsArr);
-                var name = animalsArr[0].name;
-                var age = animalsArr[0].age;
-                var breed = animalsArr[0].breeds.primary;
-                var gender = animalsArr[0].gender;
-                var url = animalsArr[0].url;
-                var description = "";
-                if (description !== null) {
-                    description = animalsArr[0].description;
+                name = animalsArr[0].name;
+                age = animalsArr[0].age;
+                breed = animalsArr[0].breeds.primary;
+                gender = animalsArr[0].gender;
+                url = animalsArr[0].url;
+                description = animalsArr[0].description;;
+                if (description === null) {
+                    description = "N/A"
+                }
+                spayedNeutered = animalsArr[0].attributes.spayed_neutered;
+                if (spayedNeutered === true) {
+                    spayedNeutered = "Yes"
                 }
                 else {
-                    description = "No Description.";
+                    spayedNeutered = "No"
                 }
                 console.log("Name " + name);
                 console.log("Age " + age);
-                console.log("Breeds " + breed);
+                console.log("Breed " + breed);
                 console.log("Gender " + gender);
                 console.log(url);
                 console.log(description);
+                console.log(spayedNeutered);
 
                 // Render photo
                 var photo = animalsArr[0].primary_photo_cropped;
-                var photoURL = "";
                 if (photo !== null) {
                     photoURL = photo.small;
                     console.log("Photo URL: " + photoURL);
@@ -52,15 +74,16 @@ $(document).ready(function () {
                 }
 
                 // Parse Contact info
-                var address = animalsArr[0].contact.address.address1;
+                address = animalsArr[0].contact.address.address1;
                 if (address == null) {
                     address = "";
                 }
                 console.log(address);
-                var city = animalsArr[0].contact.address.city;
+                city = animalsArr[0].contact.address.city;
                 console.log(city);
-                var state = animalsArr[0].contact.address.state;
+                state = animalsArr[0].contact.address.state;
                 console.log(state);
+                populateDogCards();
 
             })
             .catch(function (error) {
@@ -84,4 +107,32 @@ $(document).ready(function () {
         petfinderCall();
     })
 
+    // Function to populate Dag Cards Info
+    function populateDogCards() {
+        dogInfo.empty();
+        dogInfo.prepend(/*html*/`<div class="card horizontal dog-cards">
+                                    <div id="dog-image" class="card-image">
+                                        <img class="materialboxed" width="100" height="200"
+                                            src=${photoURL}
+                                            alt="picture of available dog">
+                                    </div>
+                                    <div id="dog-card" class="card-stacked">
+                                         <div class="card-content">
+                                            <p>Name: ${name}</p>
+                                            <p>Age: ${age}</p>
+                                            <p>Gender: ${gender}</p>
+                                            <p>Spayed/Neutered: ${spayedNeutered}</p>
+                                            <p>Location: <span>${address}</span><br>
+                                                        <span>${city}</span><br>
+                                                        <span>${state}</span>
+                                            </p>
+                                            <p>Description: ${description}</p>
+                                        </div>
+                                        <div class="card-action">
+                                            <a href=${url}>See more!</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                 <br>`);
+    }
 })
