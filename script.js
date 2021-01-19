4cfa50d1dbd7c078eb07df9a28d1a57cd27d90de
Trevor Smith <trevorsmithbanjo@gmail.com>
2021-01-18 20:15:45 -0600

    Wishlist With Saved Searches Added.
    This took me a while to work out and could be improved but I think its kind of cool. I added a save button to each search. All the dog info of the saved search card is saved in localstorage. You can click the wishlist link and it will clear the display area and display your saved searches.

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ $(document).ready(function () {
     var stateSearch = $(".stateSearch");
     var submitBtn = $("#submit-button");
     var dogInfo = $("#dog-info-container");
+    var wishlist = $("#wishlist");
 
     // Global Variables
     // User search is the string from the city state submit form.
     var userSearch = "";
     var searchStorage = [];
+    var saveSearch = {};
+
 
 
     // Petfinder API Keys
@@ -96,9 +99,9 @@ $(document).ready(function () {
             city = animalsArr[i].contact.address.city;
             state = animalsArr[i].contact.address.state;
 
-            dogInfo.prepend(/*html*/`<div class="card horizontal dog-cards">
+            dogInfo.append(/*html*/`<div class="card horizontal dog-cards">
                                     <div id="dog-image" class="card-image">
-                                        <img class="materialboxed" width="100" height="200"
+                                        <img class="materialboxed" id="photo-url-${[i]}" width="100" height="200"
                                             src=${photoURL}
                                             alt="picture of available dog">
                                     </div>
@@ -106,13 +109,13 @@ $(document).ready(function () {
                                          <div class="card-content">
                                             <p id="name-${[i]}">Name: ${name}</p>
                                             <p id="age-${[i]}">Age: ${age}</p>
-                                            <p id="gender${[i]}">Gender: ${gender}</p>
+                                            <p id="gender-${[i]}">Gender: ${gender}</p>
                                             <p id="spayed-neutered-${[i]}">Spayed/Neutered: ${spayedNeutered}</p>
                                             <p id="location-${[i]}">Location: ${address} ${city}, ${state}</p>
                                             <p id="description-${[i]}">Description: ${decodeDescription(description)}</p>
                                         </div>
                                         <div class="card-action">
-                                            <a href=${url} id="photo-url-${[i]}">See more!</a>
+                                            <a href=${url} id="url-${[i]}">See more!</a>
                                             <button class="btn waves-effect waves-light save-button" data-target="${[i]}" type="submit" name="action"
                                         color="white">Save</button>
                                         </div>
@@ -132,13 +135,77 @@ $(document).ready(function () {
             var saveSpeyedNeutered = $("#spayed-neutered-" + target).text();
             var saveLocation = $("#location-" + target).text();
             var saveDescription = $("#description-" + target).text();
-            var savePhotoUrl = $("#photo-url-" + target).attr("href");
-            console.log(savePhotoUrl);
+            var savePhotoURL = $("#photo-url-" + target).attr("src");
+            var saveURL = $("#url-" + target).attr("href");
+            saveSearch = {
+                name: saveName,
+                age: saveAge,
+                gender: saveGender,
+                spayedNeutered: saveSpeyedNeutered,
+                location: saveLocation,
+                description: saveDescription,
+                photoURL: savePhotoURL,
+                url: saveURL
+            };
+            console.dir(saveSearch);
+            searchStorage.push(saveSearch);
+            console.dir(searchStorage);
+            localStorage.setItem("saved", JSON.stringify(searchStorage));
         })
     }
+
+    // Click event for wishlist
+    wishlist.on("click", function (event) {
+        console.log($(this));
+        event.preventDefault();
+        displayWishlist(searchStorage);
+    })
 
     // Decode description function to account for apostrophes
     function decodeDescription(description) {
         return $("<span>").html(description).text();
+    }
+
+    // Function to show wishlist results
+    function displayWishlist(searchStorage) {
+        dogInfo.empty();
+        var checkStorage = JSON.parse(localStorage.getItem("saved"));
+        if (checkStorage !== null) {
+            searchStorage = checkStorage;
+        }
+        for (i = 0; i < searchStorage.length; i++) {
+            var photoURL = searchStorage[i].photoURL;
+            var name = searchStorage[i].name;
+            var age = searchStorage[i].age;
+            var gender = searchStorage[i].gender;
+            var spayedNeutered = searchStorage[i].spayedNeutered;
+            var location = searchStorage[i].location;
+            var description = searchStorage[i].description;
+            var url = searchStorage[i].url;
+
+            dogInfo.prepend(/*html*/`<div class="card horizontal dog-cards">
+                                    <div id="dog-image" class="card-image">
+                                        <img class="materialboxed" width="100" height="200"
+                                            src=${photoURL}
+                                            alt="picture of available dog">
+                                    </div>
+                                    <div id="dog-card" class="card-stacked">
+                                         <div class="card-content">
+                                            <p id="name-${[i]}">${name}</p>
+                                            <p id="age-${[i]}">${age}</p>
+                                            <p id="gender-${[i]}">${gender}</p>
+                                            <p id="spayed-neutered-${[i]}">${spayedNeutered}</p>
+                                            <p id="location-${[i]}">${location}</p>
+                                            <p id="description-${[i]}">${description}</p>
+                                        </div>
+                                        <div class="card-action">
+                                            <a href=${url} id="photo-url-${[i]}">See more!</a>
+                                            <button class="btn waves-effect waves-light save-button" data-target="${[i]}" type="submit" name="action"
+                                        color="white">Save</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                 <br>`);
+        }
     }
 })
